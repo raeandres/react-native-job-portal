956cc87492463abf671086a97e05ba702780d760
Rae-An Andres <raeandres@users.noreply.github.com>
2025-01-12 18:52:29 +0800

    Added assets in _layout.js
    Added assets in _layout.js

--- app/_layout.js
+++ app/_layout.js
@@ -7,7 +7,21 @@ SplashScreen.preventAutoHideAsync();
 const { Stack } = require("expo-router");
 
 const Layout = () => { 
-    return <Stack/>;
+
+    const [fontsLoaded] = useFonts({
+        DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+        DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+        DMRegular: require('../assets/fonts/DMSans-Regular.ttf')
+    })
+
+    const onLayoutRootView = useCallback(async () => {
+        if(fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded])
+
+    if(!fontsLoaded) return null
+    return <Stack onLayout={onLayoutRootView}/>;
 }
 
 
