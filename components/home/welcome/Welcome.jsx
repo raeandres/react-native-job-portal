734dd34a0f39a4d0c6abb5bb5a1c0e443c9bfe9d
Rae-An Andres <raeandres@users.noreply.github.com>
2025-01-12 19:01:07 +0800

    Added flatlist
    Added flatlist to show jobtypes array

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -12,33 +12,45 @@ import styles from './welcome.style'
 import { useRouter } from 'expo-router'
 import { icons, SIZES } from '../../../constants'
 
-const Welcome = () => { 
+const jobTypes = ["Full-time", "Part-time", "Contractor"];
+
+const Welcome = () => {
   return (
     <View>
       <View style={styles.container}>
         <Text style={styles.userName}>Hello</Text>
         <Text style={styles.welcomeMessage}>Find your perfect job</Text>
       </View>
-      
+
       <View style={styles.searchContainer}>
         <View style={styles.searchWrapper}>
           <TextInput
             style={styles.searchInput}
             value=""
-            onChange={() => {}}
+            onChange={() => { }}
             placeholder="What are you looking for?"
           />
         </View>
 
-        <TouchableOpacity 
+        <TouchableOpacity
           style={styles.searchBtn}
-          onPress={() => {}}>
+          onPress={() => { }}>
           <Image
             source={icons.search}
             resizeMode="contain"
             style={styles.searchBtnImage}
           />
         </TouchableOpacity>
+      </View>
+
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity>
+              <Text>{item}</Text>
+            </TouchableOpacity>
+          )} />
       </View>
     </View>
   )
