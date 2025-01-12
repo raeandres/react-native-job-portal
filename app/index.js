fe15f1d27126ebc48e5e28ff98de6195db9baf59
Rae-An Andres <raeandres@users.noreply.github.com>
2025-01-12 15:02:48 +0800

    Added <Text>

--- app/index.js
+++ app/index.js
@@ -3,7 +3,9 @@ import { View, Text } from "react-native";
 const Home = () => {
     return(
         <View>
-            Home
+            <Text>
+                Home
+            </Text>
         </View>
     )
 }
