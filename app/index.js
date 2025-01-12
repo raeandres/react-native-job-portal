5fdb3899617c73dcc7e50030451999e793d25b3e
Rae-An Andres <raeandres@users.noreply.github.com>
2025-01-12 15:57:23 +0800

    Added base components
    SafeAreaview
    
    Screenheader button left for menu
    ScreenHeader button right for profile
    Removed header title
    
    Added Scrollview
    added Welcome component
    Added Popularjobs component
    Added Nearbyjobs component

--- app/index.js
+++ app/index.js
@@ -1,12 +1,39 @@
-import { View, Text } from "react-native";
+
+import { Stack, useRouter } from 'expo-router';
+import { useState } from 'react';
+import { View, ScrollView, Text, SafeAreaView } from 'react-native';
+
+import { COLORS, icons, images, SIZES, Sizes } from '../constants';
+import { Nearbyjobs, NearbyJobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components';
 
 const Home = () => {
-    return(
-        <View>
-            <Text>
-                Home
-            </Text>
-        </View>
+
+    const router = useRouter();
+
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+            <Stack.Screen
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerLeft: () => (<ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />),
+                    headerRight: () => (<ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />),
+                    headerTitle: ""
+                }}
+            />
+
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={{
+                    flex: 1,
+                    padding: SIZES.medium
+                }}>
+                    <Welcome
+                    />
+                    <Popularjobs/>
+                    <Nearbyjobs/>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
